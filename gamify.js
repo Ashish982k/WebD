c640ce79af60a17459b87baa0b34c957583fce9b
Ashish982k <pg8355914779@gmail.com>
2025-05-02 22:21:48 +0530

    Add files via upload

--- gamify.js
+++ gamify.js
@@ -37,7 +37,7 @@ cartClose.addEventListener("click", () => {
     shopSection.classList.remove("shifted");
 });
 
-// Add to Cart functionality
+// Add to Cart wala part
 const addCart = document.querySelectorAll(".add");
 const cartContent = document.querySelector(".cart-content");
 
@@ -225,4 +225,50 @@ document.addEventListener("DOMContentLoaded", () => {
             buyer.display.style = 'none';
         });
     }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.querySelector('.login');
+    const signUpForm = document.querySelector('.login-sign-up');
+
+    const toSignUp = document.getElementById('sign-up');
+    const toLogin = document.getElementById('login');
+
+    toSignUp.addEventListener('click', (e) => {
+        e.preventDefault();
+        loginForm.style.display = 'none';
+        signUpForm.style.display = 'block';
+    });
+
+    toLogin.addEventListener('click', (e) => {
+        e.preventDefault();
+        signUpForm.style.display = 'none';
+        loginForm.style.display = 'block';
+    });
+
+    loginForm.style.display = 'block';
+    signUpForm.style.display = 'none';
+
+    function func() {
+        let name = document.getElementById("user").value;
+        let password = document.getElementById("pass").value;
+    
+        let a = "Ashish";
+        let b = "ashish@123";
+    
+        if (name === a && password === b) {
+           
+            sessionStorage.setItem("username", name);
+    
+
+            window.location.href = "gamify.html";
+        } else {
+            alert("Username or Password is INCORRECT");
+        }
+    }
+
+    document.getElementById("login-form").addEventListener("submit", function(e) {
+        e.preventDefault();
+        func();
+    });
 });
